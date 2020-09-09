c9a98bc2877e93accc6155ef542d7505df7ea11e
Jatin Malhotra <jatin05m@gmail.com>
2020-09-09 16:15:59 +0530

    Updates

---
+++ precache-manifest.9edff078ca4a07d2863e3a8752ce96d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad06e6bb8263c9b9df7cb9f90b29b59",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "875e2cefb7bcb9c4157b",
+    "url": "/Covid19Tracker/static/css/main.ee5c3c3d.chunk.css"
+  },
+  {
+    "revision": "10ba14e10c337a426876",
+    "url": "/Covid19Tracker/static/js/2.63654b94.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.63654b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875e2cefb7bcb9c4157b",
+    "url": "/Covid19Tracker/static/js/main.01d2041d.chunk.js"
+  },
+  {
+    "revision": "9fa1dc6f1e487a84ceb5",
+    "url": "/Covid19Tracker/static/js/runtime-main.babf432b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19Tracker/static/media/image.d7265326.png"
+  }
+]);
